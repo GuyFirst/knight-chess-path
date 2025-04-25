fbe8a5324f1c6d7cc191299f58bf2b39c4ad0039
Guy First <guyfi2@mta.ac.il>
2025-04-25 04:31:34 +0300

    handled spacing in chess.h

--- chess.h
+++ chess.h
@@ -26,18 +26,18 @@ typedef struct _chessPosList {
 } chessPosList;
 
 //------------ FUNC DECLARATIONS ------------//
-void processMoves(chessPos knightPos, chessPos** knightMovesArr, int* curr, int* phSize); //checks if the pos is valid and insert it to arr
-chessPos* arrForKnightPos(chessPos knightPos, int* logSize); //builds an array of next possible knight positions for curr chessPos 
-chessPosArray*** validKnightMoves(); //the main func the creates and returns the chessboard arrayfrom Q1
-bool isValidMove(chessPos optionalKnightPos); //checks if the knight can move to the suggested spot
-void createATable(chessPosList lst, int board[COLS][ROWS]); //creates a table for the given lst
-void printTable(chessPosList lst); //prints the table
-chessPos* chessPosToBoardPos(chessPos Pos); //converts the chess positionto a position in the board
-void checkAllocation(void* ptr); //checks allocation for dynamic array
-void displayBoard(chessPosCell* curr);//removes duplicates in list
-void display(chessPosList* lst); //displaying a chess board from Q2
-bool isEmptyChessList(chessPosList lst);//check if the list is empty
-void freeArrForKnightPos(chessPos* knightMovesArr); //freeing arr for knight positions
-void freeValidKnightMoves(chessPosArray*** validMoves); //freeing the whole chess board of kninght positions
+void      	processMoves(chessPos knightPos, chessPos** knightMovesArr, int* curr, int* phSize); //checks if the pos is valid and insert it to arr
+chessPos* 	arrForKnightPos(chessPos knightPos, int* logSize); //builds an array of next possible knight positions for curr chessPos 
+chessPosArray***validKnightMoves(); //the main func the creates and returns the chessboard arrayfrom Q1
+bool 		isValidMove(chessPos optionalKnightPos); //checks if the knight can move to the suggested spot
+void 		createATable(chessPosList lst, int board[COLS][ROWS]); //creates a table for the given lst
+void 		printTable(chessPosList lst); //prints the table
+chessPos* 	chessPosToBoardPos(chessPos Pos); //converts the chess positionto a position in the board
+void 		checkAllocation(void* ptr); //checks allocation for dynamic array
+void 		displayBoard(chessPosCell* curr);//removes duplicates in list
+void 		display(chessPosList* lst); //displaying a chess board from Q2
+bool 		isEmptyChessList(chessPosList lst);//check if the list is empty
+void 		freeArrForKnightPos(chessPos* knightMovesArr); //freeing arr for knight positions
+void 		freeValidKnightMoves(chessPosArray*** validMoves); //freeing the whole chess board of kninght positions
 
 
